2d6e704cd7c9fd748e75988a14417905697af2e0
Madhura Besagarahalli Nagaraju <110226025+b-nM77@users.noreply.github.com>
2025-05-17 20:48:04 +0100

    Initial commit

---
+++ src/data/generated/bib.js
@@ -0,0 +1,116 @@
+﻿define({ entries : {
+    "Alatrany2022Machine": {
+        "author": "Abbas Saad Alatrany and Abir Jaafar Hussain and Jamila Mustafina and Dhiya Al-Jumeily",
+        "doi": "10.1109/ACCESS.2022.3182543",
+        "journal": "IEEE Access",
+        "keywords": "Alzheimer\u2019s disease, GWAS, SNP, machine learning, systematic review",
+        "publisher": "IEEE",
+        "title": "Machine Learning Approaches and Applications in Genome Wide Association Study for Alzheimer\u2019s Disease: A Systematic Review",
+        "type": "article",
+        "url": "https://doi.org/10.1109/ACCESS.2022.3182543",
+        "volume": "10",
+        "year": "2022"
+    },
+    "Basaia2019Automated": {
+        "author": "Silvia Basaia and Federica Agosta and Luca Wagner and Elisa Canu and Giuseppe Magnani and Roberto Santangelo and Massimo Filippi",
+        "doi": "10.1016/j.nicl.2018.101645",
+        "journal": "NeuroImage: Clinical",
+        "keywords": "deep learning, MRI, Alzheimer's disease, classification, CNN",
+        "note": "for the Alzheimer's Disease Neuroimaging Initiative",
+        "publisher": "Elsevier",
+        "title": "Automated classification of Alzheimer's disease and mild cognitive impairment using a single MRI and deep neural networks",
+        "type": "article",
+        "url": "https://doi.org/10.1016/j.nicl.2018.101645",
+        "volume": "21",
+        "year": "2019"
+    },
+    "Chung2020Automatic": {
+        "author": "Beom Sun Chung and Jin Seo Park",
+        "doi": "10.1002/ca.23564",
+        "journal": "Clinical Anatomy",
+        "keywords": "segmentation, MRI, brain, FSL, 3D model, anatomy",
+        "publisher": "Wiley",
+        "title": "Automatic segmentation of true color sectioned images using FMRIB Software Library: First trial in brain, gray matter, and white matter",
+        "type": "article",
+        "url": "https://doi.org/10.1002/ca.23564",
+        "volume": "33",
+        "year": "2020"
+    },
+    "Dara2023Alzheimers": {
+        "author": "Omer Asghar Dara and Jose Manuel Lopez-Guede and Hasan Issa Raheem and Javad Rahebi and Ekaitz Zulueta and Unai Fernandez-Gamiz",
+        "doi": "10.3390/app13148298",
+        "journal": "Applied Sciences",
+        "keywords": "survey, machine learning, Alzheimer's disease, feature selection, MRI",
+        "publisher": "MDPI",
+        "title": "Alzheimer\u2019s Disease Diagnosis Using Machine Learning: A Survey",
+        "type": "article",
+        "url": "https://doi.org/10.3390/app13148298",
+        "volume": "13",
+        "year": "2023"
+    },
+    "Jeyalakshmi2024Neuroimaging": {
+        "author": "Jeyalakshmi J and Lokesh Kumar J and Vishnu S",
+        "booktitle": "2024 10th International Conference on Communication and Signal Processing (ICCSP)",
+        "keywords": "Alzheimer\u2019s, ADNI, EfficientNet, deep learning, neuroimaging, MRI, fMRI",
+        "publisher": "IEEE",
+        "title": "Neuroimaging Analysis for Early Diagnosis of ADNI Progression with EfficientNet",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.1109/ICCSP60870.2024.10543702",
+        "year": "2024"
+    },
+    "Mahyoub2018Comparison": {
+        "author": "Mohamed Mahyoub and Martin Randles and Thar Baker and Po Yang",
+        "journal": "2018 11th International Conference on Developments in eSystems Engineering (DeSE)",
+        "keywords": "Alzheimer\u2019s disease, machine learning, risk factors, classification, ADNI",
+        "publisher": "IEEE",
+        "title": "Comparison Analysis of Machine Learning Algorithms to Rank Alzheimer\u2019s Disease Risk Factors by Importance",
+        "type": "article",
+        "url": "https://doi.org/10.1109/DeSE.2018.00008",
+        "year": "2018"
+    },
+    "Qiu2022Multimodal": {
+        "author": "Shangran Qiu and Matthew I. Miller and Prajakta S. Joshi and Joyce C. Lee and Chonghua Xue and Yunruo Ni and others",
+        "doi": "10.1038/s41467-022-31037-5",
+        "journal": "Nature Communications",
+        "keywords": "deep learning, dementia, Alzheimer\u2019s disease, multimodal data, MRI, clinical diagnostics",
+        "number": "3404",
+        "publisher": "Nature",
+        "title": "Multimodal deep learning for Alzheimer\u2019s disease dementia assessment",
+        "type": "article",
+        "url": "https://doi.org/10.1038/s41467-022-31037-5",
+        "volume": "13",
+        "year": "2022"
+    },
+    "Thibeau2021MRI": {
+        "author": "Elina Thibeau-Sutre and Baptiste Couvy-Duchesne and Didier Dormont and Olivier Colliot and Ninon Burgos",
+        "journal": "Proceedings of the IEEE International Symposium on Biomedical Imaging (ISBI)",
+        "keywords": "MRI bias, field strength, Alzheimer\u2019s disease, ADNI, deep learning",
+        "publisher": "IEEE",
+        "title": "MRI Field Strength Predicts Alzheimer\u2019s Disease: a Case Example of Bias in the ADNI Data Set",
+        "type": "article",
+        "url": "https://doi.org/10.1109/ISBI52829.2022.9761504",
+        "year": "2021"
+    },
+    "Tsang2008Comparison": {
+        "author": "O. Tsang and A. Gholipour and N. Kehtarnavaz and I. Panahi and K. Gopinath and R. Briggs",
+        "journal": "Conference Proceedings of the IEEE Engineering in Medicine and Biology Society",
+        "keywords": "segmentation, neuroimaging, MRI, FSL, SPM, gray matter, white matter",
+        "publisher": "IEEE",
+        "title": "Comparison of Tissue Segmentation Algorithms in Neuroimage Analysis Software Tools",
+        "type": "article",
+        "url": "https://doi.org/10.1109/IEMBS.2008.4650068",
+        "year": "2008"
+    },
+    "Varesi2022Blood": {
+        "author": "Angelica Varesi and Adelaide Carrara and Vitor Gomes Pires and Valentina Floris and Elisa Pierella and Gabriele Savioli and Sakshi Prasad and Ciro Esposito and Giovanni Ricevuti and Salvatore Chirumbolo and Alessia Pascale",
+        "doi": "10.3390/cells11081367",
+        "journal": "Cells",
+        "keywords": "biomarkers, blood test, Alzheimer's, diagnosis, progression, miRNA",
+        "publisher": "MDPI",
+        "title": "Blood-Based Biomarkers for Alzheimer\u2019s Disease Diagnosis and Progression: An Overview",
+        "type": "article",
+        "url": "https://doi.org/10.3390/cells11081367",
+        "volume": "11",
+        "year": "2022"
+    }
+}});
